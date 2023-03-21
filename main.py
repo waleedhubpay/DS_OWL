cd2d183d068ea3d69636162b1fa7d1992dfccbb1
WaleedHashmi <waleedhashmi@nyu.edu>
2023-03-20 22:31:01 -0700

    added mutliprocessing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
 from src.pipeline_class import Pipeline
+import multiprocessing
 
 # Set up logging
 logger = logging.getLogger(__name__)
@@ -8,12 +9,28 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
 
     # Adding descriptive comments to the code
-    logger.info("Starting the Iris MLOps pipeline.")
-    logger.info("Press Ctrl+C to stop the pipeline.")
+    logger.info("Starting the OWL.")
+    logger.info("Press Ctrl+C to stop the pipeline(s).")
     logger.info("For documentation: https://github.com/waleedhubpay/DS_OWL\n")
 
     # initiate the pipeline(s)
-    iris = Pipeline()
+    process_1 = Pipeline()
+    process_2 = Pipeline()
 
-    # schedule the pipeline(s)
-    iris.schedule()
+    # define the pipeline(s) as Process object(s)
+    process_1 = multiprocessing.Process(target=process_1.schedule)
+    process_2 = multiprocessing.Process(target=process_2.schedule)
+
+    # Start the processes
+    process_1.start()
+    process_2.start()
+
+    processes = [process_1, process_2]
+
+    try:
+        for process in processes:
+            process.join()
+    except KeyboardInterrupt:
+        logger.info("Terminating Pipelines")
+        for process in processes:
+            process.terminate()
